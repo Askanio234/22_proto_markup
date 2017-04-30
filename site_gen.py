eb83c9fee574ca5295f5d2c5ac7d817aacd5945f
Artem Chernyaev <askanio234@gmail.com>
2017-04-30 12:37:20 +0300

    Add basic generation for index.html via staticjinja

--- site_gen.py
+++ site_gen.py
@@ -1,15 +1,25 @@
 import staticjinja
 
 
-requests_data = {
-    'title': 'Заявки',
-    "indents_title": "Заявки",
-    'user_name': 'Леонид Федорович',
-    'indents': 10
+index_data = {
+	"title": "Главная",
+	"indents_title": "Свежие заявки",
+	"user_name": "Леонид Федорович",
+	"indents": 10
 }
 
-if __name__ == '__main__':
-    site = staticjinja.make_site(outpath='site', contexts=[
-        ('indents.html', requests_data)
+
+requests_data = {
+    "title": "Заявки",
+    "indents_title": "Заявки",
+    "user_name": "Леонид Федорович",
+    "indents": 10
+}
+
+
+if __name__ == "__main__":
+    site = staticjinja.make_site(outpath="site", contexts=[
+        ('indents.html', requests_data),
+        ("index.html", index_data)
     ])
     site.render()
