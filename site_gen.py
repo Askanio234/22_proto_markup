80c08fa63037c0643a2cab8fc2d1369a73166a14
Artem Chernyaev <askanio234@gmail.com>
2017-04-30 12:18:34 +0300

    Add basic templates for indents.html & generation via staticjinja

--- site_gen.py
+++ site_gen.py
@@ -1,1 +1,16 @@
 import staticjinja
+
+
+requests_data = {
+    'title': 'Заявки',
+    "indents_title": "Заявки",
+    'user_name': 'Леонид Федорович',
+    'indents': 10,
+    "final_button_name": "Показать еще"
+}
+
+if __name__ == '__main__':
+    site = staticjinja.make_site(outpath='site', contexts=[
+        ('indents.html', requests_data)
+    ])
+    site.render()
